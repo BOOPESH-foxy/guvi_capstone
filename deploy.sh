5a67d4ff599ff9fafded4d05b428bb06f59ef95d
BOOPESH-foxy <boopesh.mc@gmail.com>
2024-04-10 15:48:12 +0530

    renamed user and added images

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker tag capstone-image 989407/capstone-image:latest
 docker push 989407/capstone-image:latest
 
 # SSH into the server and pull the latest image
-ssh ec2-user@server "docker pull /capstone-image:latest"
+ssh ec2-user@ip-172-31-11-137 "docker pull /capstone-image:latest"
 
 # Restart the Docker container on the server
-ssh ec2-user@server "docker-compose -f /path/to/docker-compose.yml up -d"
+ssh ec2-user@ip-172-31-11-137 "docker-compose -f /path/to/docker-compose.yml up -d"
